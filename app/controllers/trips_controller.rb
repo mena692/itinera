b41a04ec9b20faf7dbb145e13a53d2041a94d38c
Angelo Garcia <angeloqgarcia@gmail.com>
2026-08-25 17:55:55 +0200

    started on trip - controller, route, view, policy

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,9 @@
 class TripsController < ApplicationController
+  def show
+    @trip = Trip.find(params[:id])
+    authorize @trip
+    @trip_days = @trip.trip_days.order(date: :asc)
+    @trip_day = @trip_days[params[:day].to_i - 1]
+    # @activity = Activity.find(params[:id])
+  end
 end
