592ac6db6014f21bd1cfdf42afcd0dc89e92a13d
Angelo Garcia <angeloqgarcia@gmail.com>
2026-08-31 11:33:22 +0200

    added create activity + fixed relevant view pages

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,24 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: %i[show edit update destroy]
 
+  def new
+    set_trip_and_day
+    @activity = @trip_day.activities.new
+    authorize @activity
+  end
+
+  def create
+    set_trip_and_day
+    @activity = @trip_day.activities.new(create_params)
+    authorize @activity
+
+    if @activity.save
+      redirect_to trip_path(@trip, day: @day_number), notice: "Activity added."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     authorize @activity
     @day_number = day_number_for(@activity)
@@ -15,7 +33,7 @@ class ActivitiesController < ApplicationController
     authorize @activity
 
     if @activity.update(update_params)
-      redirect_to @activity, notice: "Activity updated."
+      redirect_to [@trip, @activity], notice: "Activity updated."
     else
       @day_number = day_number_for(@activity)
       render :edit, status: :unprocessable_entity
@@ -34,6 +52,15 @@ class ActivitiesController < ApplicationController
 
   def set_activity
     @activity = Activity.find(params[:id])
+    @trip = @activity.trip_day.trip
+  end
+
+  def set_trip_and_day
+    @trip = current_user.trips.find(params[:trip_id])
+    authorize @trip
+
+    @day_number = (params[:day] || 1).to_i
+    @trip_day = @trip.trip_days.order(:date)[@day_number - 1]
   end
 
   def day_number_for(activity)
@@ -58,10 +85,19 @@ class ActivitiesController < ApplicationController
     activity_params
   end
 
+  # The new form only collects a time of day, so the submitted start_time/end_time
+  # are merged onto the trip day's date before mass-assignment.
+  def create_params
+    raw = params.require(:activity)
+    raw[:start_date] = combine_date_and_time(@trip_day.date, raw.delete(:start_time))
+    raw[:end_date] = combine_date_and_time(@trip_day.date, raw.delete(:end_time))
+    activity_params
+  end
+
   def combine_date_and_time(original_datetime, time_string)
     return original_datetime if time_string.blank?
 
     hour, minute = time_string.split(":").map(&:to_i)
-    original_datetime.change(hour: hour, min: minute)
+    original_datetime.in_time_zone.change(hour: hour, min: minute)
   end
 end
